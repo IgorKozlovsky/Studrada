3fbe2e11dea5100e4e9dfaab25f4be63a84bffd9
Kozlovsky Igor <i.kozlovsky2004@gmail.com>
2023-01-27 19:37:26 +0200

    Merge conflict

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Banner from 'src/components/Banner'
 import Articles from 'src/modules/Articles'
+import ArticleInfo from 'src/modules/Layout/components/ArticleIndo/ArticleInfo'
 
 const Home = () => {
   return (
     <>
+      <Banner />
+      <ArticleInfo />
       <Articles />
     </>
   )
