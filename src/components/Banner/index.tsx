5bd97b0e25ebeaf41369a3a450cedf3d96819086
HotsykVictor <vitia2372087@gmail.com>
2023-02-19 22:08:06 +0200

    Added Banner styled-component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,25 +1,13 @@
-import Flex from 'src/ui/Flex'
-import img from 'src/static/banner.png'
+import StyledBanner from 'src/components/Banner/styles'
 import Text from 'src/ui/Text'
 
 const Banner = () => {
   return (
-    <Flex
-      sx={{
-        backgroundImage: `url(${img})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
+    <StyledBanner>
       <Text variant={'bannerText'} sx={{ textAlign: 'center ' }}>
         Студенська рада <br /> НУ &quot;ОЮА&quot;
       </Text>
-    </Flex>
+    </StyledBanner>
   )
 }
 
